99d897faebd38dc051d795b96e64c61170dd2c5d
futpib <futpib@gmail.com>
2017-03-30 11:39:30 +0300

    package.json and UMD

--- dataTables.rowsGroup.js
+++ dataTables.rowsGroup.js
@@ -38,7 +38,37 @@
  *     
  */
 
-(function($){
+(function( factory ) {
+	"use strict";
+
+	if ( typeof define === 'function' && define.amd ) {
+		// AMD
+		define( ['jquery'], function ( $ ) {
+			return factory( $, window, document );
+		} );
+	}
+	else if ( typeof exports === 'object' ) {
+		// CommonJS
+		module.exports = function (root, $) {
+			if ( ! root ) {
+				root = window;
+			}
+
+			if ( ! $ ) {
+				$ = typeof window !== 'undefined' ?
+					require('jquery') :
+					require('jquery')( root );
+			}
+
+			return factory( $, root, root.document );
+		};
+	}
+	else {
+		// Browser
+		factory( jQuery, window, document );
+	}
+}
+(function($, window, document){
 
 ShowedDataSelectorModifier = {
 	order: 'current',
@@ -266,7 +296,7 @@ $(document).on( 'init.dt', function ( e, settings ) {
 	}
 } );
 
-}(jQuery));
+}));
 
 /*
 
